ec13865e7d94913e3f9ffd0854ddf2ab744183ba
1iper <160356753+1iper@users.noreply.github.com>
2025-12-23 16:07:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import telebot
 from bot_handler import BotHandler
 
-TOKEN = '8490919762:AAFH6CBEy6xE160WWiQ6UjegB_RZBzxeYXM'  # ← замените на свой
+TOKEN = '8490919762:AAFH6CBEy6xE160WWiQ6UjegB_RZBzxeYXM' 
 
 bot = telebot.TeleBot(TOKEN)
 handler = BotHandler(bot)
@@ -14,4 +14,5 @@ def callback_handler(call):
 
 if __name__ == '__main__':
     print("Бот запущен...")
+
     bot.polling(non_stop=True, interval=0)
